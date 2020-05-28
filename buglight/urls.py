7aa162ff9b6f3cfcb0adaf437cd825372b43e004
geminerald <zacdwarner@gmail.com>
2020-05-28 10:18:53 +0000

    added basic bug home page with urls, views and html

--- buglight/urls.py
+++ buglight/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as account_urls
 from home import urls as home_urls
+from bugs import urls as bugs_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(account_urls)),
     url(r'^', include(home_urls)),
+    url(r'^bugs/', include(bugs_urls))
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Bug
 
 # Create your views here.
+
+
+def all_bugs(request):
+    bugs = Bug.objects.all()
+    return render(request, 'bugs.html', {'bugs':bugs})
