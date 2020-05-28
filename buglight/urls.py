03c504279e45621692952b78b10e5d06d92808f6
geminerald <zacdwarner@gmail.com>
2020-05-28 09:35:26 +0000

    made home page in home app and updated urls

--- buglight/urls.py
+++ buglight/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as account_urls
+from home import urls as home_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(account_urls)),
+    url(r'^', include(home_urls)),
 ]
