b5dd5412e5a3d9737c0af1d90454529053f50537
geminerald <zacdwarner@gmail.com>
2020-05-29 11:23:00 +0000

    updated background of responsive nave

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, about
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^about$', about, name='about')
 ]
